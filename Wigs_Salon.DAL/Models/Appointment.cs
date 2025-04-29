b0f6a8098541f7196111d952718156caf7402e22
User <User@DESKTOP-9DJ1STV>
2025-04-29 14:20:52 +0300

    Add project files.

---
+++ Wigs_Salon.DAL/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wigs_Salon.DAL.Models;
+
+public partial class Appointment
+{
+    public int AppointmentId { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public DateOnly AppointmentDate { get; set; }
+
+    public TimeOnly AppointmentTime { get; set; }
+
+    public int? CancellationFee { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual Employee Employee { get; set; } = null!;
+
+    public virtual ICollection<Service> Services { get; set; } = new List<Service>();
+}
---
+++ Wigs_Salon.DAL/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wigs_Salon.DAL.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public string WorkDays { get; set; } = null!;
+
+    public TimeOnly StartHour { get; set; }
+
+    public TimeOnly EndHour { get; set; }
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+}
